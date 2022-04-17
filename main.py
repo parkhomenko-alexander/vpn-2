c141b6e8e700286287c4fb839f48f16210bb4e76
sanya <sanec_290600@mail.ru>
2022-04-18 00:14:31 +1000

    i

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from nfstream import NFStreamer
 from datetime import datetime
-from tabulate import tabulate
 
 
 class DataAnalyzer:
@@ -21,7 +20,7 @@ class DataAnalyzer:
       return start, end
     
     def get_data(self):
-      return tabulate(self.streamer)
+      return self.streamer.to_markdown()
 
     def get_unique_data(self, column):
       res_df = self.streamer.copy(deep=True)
@@ -44,10 +43,10 @@ class DataAnalyzer:
     
     def get_report(self):
         with open('README.md', 'w',  encoding='utf-8') as f:
-            f.write("# Отчет по лабораторной работе 3.2\n### Выполнили:\nГубенко Иван\n Пархоменко Александр\n Чуйко Михаил\n\n")
-            f.write(f"#### Файл захвата: [{self.file_name}]({self.file_name})\n\n")
-            f.write(f"#### 1 Наличие VPN: {self.vpn_used()}\n\n")
-            f.write(f"#### 2.1 Вывод информации о данных:['src_ip','dst_ip','bidirectional_packets','bidirectional_bytes','application_name','application_category_name']: {self.get_data()}\n\n")
+            f.write("#  Отчет по лабораторной работе 3.2\n### Выполнили:\nГубенко Иван\n Пархоменко Александр\n Чуйко Михаил\n\n")
+            f.write(f"####  Файл захвата: [{self.file_name}]({self.file_name})\n\n")
+            f.write(f"#### 1    Наличие VPN: {self.vpn_used()}\n\n")
+            f.write(f"#### 2.1  Вывод информации о данных:['src_ip','dst_ip','bidirectional_packets','bidirectional_bytes','application_name','application_category_name']: {self.get_data()}\n\n")
             # f.write(f"####2.2 Уникальные значения ['src_ip']: {self.get_unique_data()}\n\n")
 
 
