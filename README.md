bdfea56ba2be45b813f7e71ee8e67d1bef80fd30
sanya <sanec_290600@mail.ru>
2022-04-17 23:43:03 +1000

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Отчет по лабораторной работе 3.2
+### Выполнили:
+Губенко Иван
+Пархоменко Александр
+Чуйко Михаил
+
+####Файл захвата: [data/all_3.pcapng](data/all_3.pcapng)
+
+####1 Наличие VPN: VPN используется
+
+####2.1 Вывод информации о данных:['src_ip','dst_ip','bidirectional_packets','bidirectional_bytes','application_name','application_category_name']:                          src_ip           dst_ip  bidirectional_packets  bidirectional_bytes application_name application_category_name
+0                 74.125.24.132  192.168.192.133                      1                   60       TLS.Google                       Web
+1                   20.42.65.92  192.168.192.133                      5                 2904        TLS.Azure                     Cloud
+2                 74.125.24.132  192.168.192.133                      1                   60       TLS.Google                       Web
+3                  20.197.71.89  192.168.192.133                      4                  820        TLS.Azure                     Cloud
+4                149.154.167.51  192.168.192.133                     11                  660     TLS.Telegram                      Chat
+...                         ...              ...                    ...                  ...              ...                       ...
+1044            192.168.192.133   159.223.54.210                   6476              5417452          OpenVPN                       VPN
+1045  fe80::e08b:1704:7c42:81b0         ff02::16                     13                 1210           ICMPV6                   Network
+1046            192.168.192.133  192.168.192.255                      6                 1296    NetBIOS.SMBv1                    System
+1047  fe80::e08b:1704:7c42:81b0          ff02::c                     14                 9604              WSD                   Network
+1048            192.168.192.133      224.0.0.252                      1                   75            LLMNR                   Network
+
+[1049 rows x 6 columns]
+
---
+++ main.py
@@ -0,0 +1,58 @@
+from nfstream import NFStreamer
+from datetime import datetime
+
+class DataAnalyzer:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.streamer = NFStreamer(source = file_name)
+        self.streamer = self.streamer.to_pandas(columns_to_anonymize=[])
+        self.columns=['src_ip', 'dst_ip', 'bidirectional_packets', 'bidirectional_bytes', 'application_name', 'application_category_name']
+        self.unique_columns =  ['src_ip','dst_ip', 'application_name']
+
+        for c in self.streamer.columns:
+          if c not in self.columns:
+            del self.streamer[c]
+
+    def capturing_time(self):
+      start = datetime.fromtimestamp(self.streamer['bidirectional_first_seen_ms'].min()/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+      end = datetime.fromtimestamp(self.streamer['bidirectional_last_seen_ms'].max()/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+      return start, end
+    
+    def get_data(self):
+      return self.streamer.to_markdown()
+
+    def get_unique_data(self, column):
+      res_df = self.streamer.copy(deep=True)
+      res_df = res_df[self.unique_columns]
+      res_df.drop_duplicates(subset=column, inplace=True)
+      return res_df.to_markdown()
+     
+    def vpn_used(self):
+      if 'VPN' in self.streamer['application_category_name'].unique():
+        return 'VPN используется'
+      else:
+        return 'VPN не используется'
+
+    def helpful_information(self):
+      res_df = self.streamer.copy(deep=True)
+      res_df = res_df[['application_name', 'application_category_name', 'bidirectional_bytes']]
+      res_df = res_df.groupby(['application_name', 'application_category_name'], as_index=False)[['bidirectional_bytes']].sum().sort_values(['bidirectional_bytes'], ascending=False)
+      return res_df.to_markdown()
+
+    
+    def get_report(self):
+        with open('README.md', 'w',  encoding='utf-8') as f:
+            f.write("# Отчет по лабораторной работе 3.2\n### Выполнили:\nГубенко Иван\nПархоменко Александр\nЧуйко Михаил\n\n")
+            f.write(f"####Файл захвата: [{self.file_name}]({self.file_name})\n\n")
+            f.write(f"####1 Наличие VPN: {self.vpn_used()}\n\n")
+            f.write(f"####2.1 Вывод информации о данных:['src_ip','dst_ip','bidirectional_packets','bidirectional_bytes','application_name','application_category_name']: {self.get_data()}\n\n")
+            # f.write(f"####2.2 Уникальные значения ['src_ip']: {self.get_unique_data()}\n\n")
+
+
+
+
+
+
+if __name__ == '__main__':
+    streamer = DataAnalyzer('data/all_3.pcapng')
+    streamer.get_report()
